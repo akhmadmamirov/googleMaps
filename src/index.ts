017b1626f255b5cf2568bca1e863a66cc86f48ca
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-12-17 13:23:19 -0500

    adding google maps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user  = new User()
-const company = new Company()
+/// <reference types="@types/google.maps" />
 
-console.log(user)
-console.log(company)
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom : 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+})
+
+
+
